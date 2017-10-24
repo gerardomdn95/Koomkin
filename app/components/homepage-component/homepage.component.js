562c865f221ea50502e6d300034338964644d733
gerardomdn95 <gerardo.mdn95@gmail.com>
2017-10-24 17:40:36 -0500

    simple scroll animations

--- app/components/homepage-component/homepage.component.js
+++ app/components/homepage-component/homepage.component.js
@@ -14,6 +14,21 @@
     function homeCtrl() {
         var vm = this;
 
+        $('.aniview').AniView();
+        
+        var $phone = $('.phone');
+        var $up = $('.up');
+        var $down = $('.down');
+
+        var $win = $(window);
+
+        $win.on('scroll', function(){
+            var top = $win.scrollTop() / 3;
+            $up.css('transform', 'translateY(' + top * 5 +'px)');
+            $down.css('transform', 'translateY(' + top * -5 +'px)');
+            $phone.css('transform', 'translateY(' + top * 5 +'px)'); //'rotate(' + top + 'deg)');
+        });
+
         $(document).ready(function () {
             $('ul.tabs').tabs();
         });
