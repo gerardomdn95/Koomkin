5939f0f13fa13ba6280ed5287949ce240ac73a0a
gerardomdn95 <gerardo.mdn95@gmail.com>
2017-10-31 17:39:27 -0600

    phone animation

--- app/components/homepage-component/homepage.component.js
+++ app/components/homepage-component/homepage.component.js
@@ -16,21 +16,27 @@
 
         $('.aniview').AniView();
 
-        var $cintaUp = $('.cintaUp');
-        var $cintaDown = $('.cintaDown');
-        var $logo = $('#logo');
         var $up = $('.up');
         var $down = $('.down');
+        var $sec2 = $('#sec2');
 
         var $win = $(window);
 
-        $win.on('scroll', function(){
+        $win.on('scroll', function () {
             var top = $win.scrollTop() / 3;
-
-            $up.css('transform', 'translateX(' + top * .3 +'px)');
-            $down.css('transform', 'translateX(' + top * -.3 +'px)');
-            $phone.css('transform', 'translateY(' + top * 5 +'px)'); //'rotate(' + top + 'deg)');
+            $up.css('transform', 'translateX(' + top * .3 + 'px)');
+            $down.css('transform', 'translateX(' + top * -.3 + 'px)');
+            //'rotate(' + top + 'deg)');
         });
+
+        var options = [
+            {
+                selector: '.sec2', offset: 500, callback: function (el) {
+                    Materialize.fadeInImage($(el));
+                }
+            }
+        ];
+        Materialize.scrollFire(options);
 
         $(document).ready(function () {
             $('ul.tabs').tabs();
