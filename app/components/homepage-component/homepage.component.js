1a6acb96c16228915420a35dc5fcf19aadf0c9b0
gerardomdn95 <gerardo.mdn95@gmail.com>
2017-11-17 16:43:28 -0600

    css miification

--- app/components/homepage-component/homepage.component.js
+++ app/components/homepage-component/homepage.component.js
@@ -20,11 +20,23 @@
         var $down2 = $('.down2');
 
         var $win = $(window);
-
+        
         $(document).ready(function () {
             $('.parallax').parallax();
             $('.modal').modal();
             $(".button-collapse").sideNav();
+            $('a[href^="#"]').on('click',function (e) {
+                e.preventDefault();
+        
+                var target = this.hash,
+                $target = $(target);
+        
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top
+                }, 900, 'swing', function () {
+                    window.location.hash = target;
+                });
+            });
             $('#second').mouseover(function () {
                 $('.secondElements').addClass("visible animated zoomIn");
             });
